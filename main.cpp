2b8c72a82ad14dc6e486718490cf2a4d09346dd5
k022g0055 <k022g0055@g.neec.ac.jp>
2024-09-30 15:28:56 +0900

    hello,worldをfor文に置き換え

--- main.cpp
+++ main.cpp
@@ -4,7 +4,13 @@
 
 int main(int argc, char* argv[]) {
 
-	printf("Hello,World!\n");
+	//argc‚Ì”‚¾‚¯ŒJ‚è•Ô‚·
+	for (int i = 0; i < argc; i++) {
+		//•¶š—ñargv‚Ìi”Ô–Ú‚ğ•\¦
+		printf(argv[i]);
+		//‰üs
+		printf("\n");
+	}
 
 	system("pause");
 	return 0;
