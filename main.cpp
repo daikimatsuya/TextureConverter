9e96a6227520fd405e7bf7c21ba016af447764bf
k022g0055 <k022g0055@g.neec.ac.jp>
2024-10-21 14:31:09 +0900

    02_04

--- main.cpp
+++ main.cpp
@@ -1,16 +1,38 @@
 #include <cstdio>
 #include <cstdlib>
+#include <cassert>
+#include "TextureConverter.h"
+#include <Windows.h>
 
+enum Argument{
+	kApplicationPath,	//アプリケーションのパス
+	kFilePath,				//渡されたファイルのパス
+	NumArgument
+};
 
 int main(int argc, char* argv[]) {
 
-	//argcの数だけ繰り返す
-	for (int i = 0; i < argc; i++) {
-		//文字列argvのi番目を表示
-		printf(argv[i]);
-		//改行
-		printf("\n");
-	}
+	assert(argc >= NumArgument);
+
+	//COM　ライブラリの初期化
+	HRESULT hr = CoInitializeEx(nullptr, COINIT_MULTITHREADED);
+	assert(SUCCEEDED(hr));
+
+	//テクスチャコンバータ
+	TextureConverter converter;
+	//テクスチャ変換
+	converter.ConvertTextureWICToDDS(argv[kFilePath]);
+
+	////argcの数だけ繰り返す
+	//for (int i = 0; i < argc; i++) {
+	//	//文字列argvのi番目を表示
+	//	printf(argv[i]);
+	//	//改行
+	//	printf("\n");
+	//}
+	
+	//COM　ライブラリの終了
+	CoUninitialize();
 
 	system("pause");
 	return 0;
