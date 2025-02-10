eba853decc5487a546a87d059d82823a66588693
k022g0055 <k022g0055@g.neec.ac.jp>
2025-02-10 20:08:51 +0900

    動くようになった

--- main.cpp
+++ main.cpp
@@ -15,8 +15,8 @@ int main(int argc, char* argv[]) {
 	assert(argc >= NumArgument);
 
 	//COM　ライブラリの初期化
-	//HRESULT hr = CoInitializeEx(nullptr, COINIT_MULTITHREADED);
-	//assert(SUCCEEDED(hr));
+	HRESULT hr = CoInitializeEx(nullptr, COINIT_MULTITHREADED);
+	assert(SUCCEEDED(hr));
 
 	//テクスチャコンバータ
 	TextureConverter converter;
